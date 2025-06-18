9dbe7dc8d5b5b4eab511a4c63e485a6d6b259dcb
Kevin Gori <kcg25@cam.ac.uk>
2025-06-18 16:12:53 +0100

    Fix formatting and unused variable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn health_check(req: HttpRequest) -> impl Responder {
+async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
