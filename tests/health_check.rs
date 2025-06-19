5199cf2455a0008e83f10d140e646d2651337675
Kevin Gori <kcg25@cam.ac.uk>
2025-06-19 20:37:38 +0100

    Subscriptions POST route tests pass

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,3 +24,51 @@ fn spawn_app() -> String {
 
     format!("http://127.0.0.1:{port}")
 }
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let route = format!("{}/subscriptions", &app_address);
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&route)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let route = format!("{}/subscriptions", &app_address);
+        let response = client
+            .post(&route)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not respond with 400 when the payload was {}.",
+            error_message
+        );
+    }
+}
