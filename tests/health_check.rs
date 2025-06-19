f703c8c021d2ccbee09a366829cb6348e3ab0783
Kevin Gori <kcg25@cam.ac.uk>
2025-06-19 15:54:49 +0100

    Refactor to use a random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,26 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
+    let route = format!("{}/health_check", &address);
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&route)
         .send()
         .await
         .expect("Failed to send request");
 
     assert!(response.status().is_success(), "Health check failed");
-    assert_eq!(
-        Some(0),
-        response.content_length(),
-        "Content length should be 0"
-    );
+    assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
 }
