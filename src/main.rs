65772979576006f56e4e507c90b439ac847e8d58
Kevin Gori <kcg25@cam.ac.uk>
2025-06-22 17:37:08 +0100

    subscriptions.rs uses sqlx to make a query on the database. Tests need updating.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -6,9 +7,12 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     // Load the config
     let settings = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&settings.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", settings.application_port);
 
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
